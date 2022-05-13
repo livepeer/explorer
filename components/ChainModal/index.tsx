d2a2178b9aad23b7e14042d8128a9156b9d34e73
Chase Adams <c@cadams.io>
2022-05-13 13:19:38 -0700

    fix: remove unused modal

--- components/ChainModal/index.tsx
+++
@@ -1,8 +0,0 @@
-
-import { ChainModal as ChainModalRainbowKit, ChainModalProps } from "@rainbow-me/rainbowkit/dist/components/ChainModal/ChainModal";
-
-const ChainModal = (props: ChainModalProps) => {
-  return <ChainModalRainbowKit {...props} />;
-};
-
-export default ChainModal;
