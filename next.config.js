80d829027d0b1ed214c95c36e568800f52bd1906
Chase Adams <c@cadams.io>
2022-07-21 18:13:30 -0700

    fix: attempted fix for api route

--- next.config.js
+++ next.config.js
@@ -12,40 +12,40 @@ const nextConfig = {
     });
     return config;
   },
-  async redirects() {
-    return [
-      {
-        source: "/accounts/:slug",
-        destination: "/accounts/:slug/delegating",
-        permanent: false,
-      },
-      {
-        source: "/transcoders",
-        destination: "/orchestrators",
-        permanent: false,
-      },
-      {
-        source: "/accounts/:account/transcoding",
-        destination: "/accounts/:account/orchestrating",
-        permanent: false,
-      },
-      {
-        source: "/accounts/:account/campaign",
-        destination: "/accounts/:account/orchestrating",
-        permanent: false,
-      },
-      {
-        source: "/accounts/:account/staking",
-        destination: "/accounts/:account/delegating",
-        permanent: false,
-      },
-      {
-        source: "/accounts/:account/overview",
-        destination: "/accounts/:account/delegating",
-        permanent: false,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/accounts/:slug",
+  //       destination: "/accounts/:slug/delegating",
+  //       permanent: false,
+  //     },
+  //     {
+  //       source: "/transcoders",
+  //       destination: "/orchestrators",
+  //       permanent: false,
+  //     },
+  //     {
+  //       source: "/accounts/:account/transcoding",
+  //       destination: "/accounts/:account/orchestrating",
+  //       permanent: false,
+  //     },
+  //     {
+  //       source: "/accounts/:account/campaign",
+  //       destination: "/accounts/:account/orchestrating",
+  //       permanent: false,
+  //     },
+  //     {
+  //       source: "/accounts/:account/staking",
+  //       destination: "/accounts/:account/delegating",
+  //       permanent: false,
+  //     },
+  //     {
+  //       source: "/accounts/:account/overview",
+  //       destination: "/accounts/:account/delegating",
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = withMDX(nextConfig);
