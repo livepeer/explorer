7ac87bfdabd28e9ab7058ad1410a886845f30872
Chase Adams <c@cadams.io>
2022-07-21 18:20:45 -0700

    fix: attempted fix for api

--- next.config.js
+++ next.config.js
@@ -1,51 +1,38 @@
-require("dotenv").config();
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-});
-
 const nextConfig = {
-  webpack(config, { webpack }) {
-    config.module.rules.push({
-      test: /\.(graphql|gql)$/,
-      exclude: /node_modules/,
-      loader: "graphql-tag/loader",
-    });
-    return config;
+  async redirects() {
+    return [
+      {
+        source: "/accounts/:slug",
+        destination: "/accounts/:slug/delegating",
+        permanent: false,
+      },
+      {
+        source: "/transcoders",
+        destination: "/orchestrators",
+        permanent: false,
+      },
+      {
+        source: "/accounts/:account/transcoding",
+        destination: "/accounts/:account/orchestrating",
+        permanent: false,
+      },
+      {
+        source: "/accounts/:account/campaign",
+        destination: "/accounts/:account/orchestrating",
+        permanent: false,
+      },
+      {
+        source: "/accounts/:account/staking",
+        destination: "/accounts/:account/delegating",
+        permanent: false,
+      },
+      {
+        source: "/accounts/:account/overview",
+        destination: "/accounts/:account/delegating",
+        permanent: false,
+      },
+    ];
   },
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/accounts/:slug",
-  //       destination: "/accounts/:slug/delegating",
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: "/transcoders",
-  //       destination: "/orchestrators",
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: "/accounts/:account/transcoding",
-  //       destination: "/accounts/:account/orchestrating",
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: "/accounts/:account/campaign",
-  //       destination: "/accounts/:account/orchestrating",
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: "/accounts/:account/staking",
-  //       destination: "/accounts/:account/delegating",
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: "/accounts/:account/overview",
-  //       destination: "/accounts/:account/delegating",
-  //       permanent: false,
-  //     },
-  //   ];
-  // },
 };
 
-module.exports = withMDX(nextConfig);
+module.exports = nextConfig;
