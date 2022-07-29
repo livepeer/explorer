b9d5dc14078a33f032633ba517bc2a7f3bcbd00a
Chase Adams <c@cadams.io>
2022-07-29 12:52:43 -0700

    fix: added delegator snapshot from merkle earnings cli

--- pages/api/generateProof.tsx
+++ pages/api/generateProof.tsx
@@ -1,12 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import delegatorClaimSnapshot from "../../data/delegatorClaimSnapshot.json";
+import delegatorClaimSnapshotRinkeby from "../../data/delegatorClaimSnapshotRinkeby.json";
 import { EarningsTree } from "@lib/earningsTree";
 import { utils } from "ethers";
+import { DEFAULT_CHAIN_ID } from "@lib/chains";
+import { chain } from "wagmi";
 
 const generateProof = async (_req: NextApiRequest, res: NextApiResponse) => {
   const { account, delegate, stake, fees } = _req.body;
   // generate the merkle tree from JSON
-  const tree = EarningsTree.fromJSON(JSON.stringify(delegatorClaimSnapshot));
+  const tree = EarningsTree.fromJSON(
+    DEFAULT_CHAIN_ID === chain.arbitrum.id
+      ? JSON.stringify(delegatorClaimSnapshot)
+      : JSON.stringify(delegatorClaimSnapshotRinkeby)
+  );
 
   // generate the proof
   const leaf = utils.solidityPack(
