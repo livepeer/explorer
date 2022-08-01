a59a53a16f8ebd209cc0993042372002eecec25e
Chase Adams <c@cadams.io>
2022-08-01 10:08:43 -0700

    ci: bump build

--- pages/voting/index.tsx
+++ pages/voting/index.tsx
@@ -46,11 +46,12 @@ const Voting = () => {
   const pollInterval = 20000;
   const [polls, setPolls] = useState<PollExtended[]>([]);
   const [loading, setLoading] = useState(true);
+
+  const currentRound = useCurrentRoundData();
+
   const { data } = usePollsQuery({
     pollInterval,
   });
-
-  const currentRound = useCurrentRoundData();
 
   useEffect(() => {
     if (data && currentRound?.currentL2Block) {
