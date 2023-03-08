663d64629dbf33440c04d3ecd4c07a4578099b0d
Chase Adams <c@cadams.io>
2023-03-08 13:05:11 -0700

    fix: move to com

--- utils/ipfs.ts
+++ utils/ipfs.ts
@@ -22,7 +22,7 @@ export const catIpfsJson = async <T>(
 ): Promise<T | null> => {
   if (ipfsHash) {
     const fetchResult = await fetch(
-      `https://ipfs.livepeer.org/ipfs/${ipfsHash}`,
+      `https://ipfs.livepeer.com/ipfs/${ipfsHash}`,
       {
         method: "GET",
       }
