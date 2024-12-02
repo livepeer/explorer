3de2be7635bf3386af5a2b74d2d1df171e3f441b
Mike Zupper <70441620+mikezupper@users.noreply.github.com>
2024-12-02 06:48:31 -0500

    Livepeer.Cloud SPE - Proposal #2 (Aug 2024 - Oct 2024) - Add AI Job Support (#274)
    * Livepeer.Cloud SPE - Proposal #2 (Aug 2024 - Oct 2024) - Add AI Job Support
    
    * Update sortType to handle double and null/undefined/0 values correctly
    
    ---------
    
    Co-authored-by: 0xspeedybird <0xspeedybird@gmail.com>

---
+++ components/PerformanceListSelector/index.tsx
@@ -0,0 +1,139 @@
+import { Box, Skeleton } from "@livepeer/design-system";
+import { useAvailableInferencePipelinesData } from "hooks";
+import { ChevronDownIcon } from "@modulz/radix-icons";
+import { Pipeline } from "@lib/api/types/get-available-pipelines";
+
+interface PerformanceSelectorProps {
+  selectedPipeline: Pipeline["id"] | null;
+  setSelectedPipeline: (pipeline: Pipeline["id"] | null) => void;
+  selectedModel: string | null;
+  setSelectedModel: (model: string | null) => void;
+}
+
+const PerformanceListSelector: React.FC<PerformanceSelectorProps> = ({
+  selectedPipeline,
+  setSelectedPipeline,
+  selectedModel,
+  setSelectedModel,
+}) => {
+
+  const {data: availPipelines, isValidating} = useAvailableInferencePipelinesData();
+  const findPipelineIndex = (pipelineId) =>
+    availPipelines.pipelines.findIndex((p) => p.id === pipelineId);
+
+  const findModelIndex = (pipelineIndex, model) =>
+    availPipelines.pipelines[pipelineIndex]?.models.findIndex((m) => m === model);
+
+  const indexOfSelectedPipeline = selectedPipeline ? findPipelineIndex(selectedPipeline) : -1;
+  const indexOfSelectedModel =
+    indexOfSelectedPipeline !== -1 && selectedModel
+      ? findModelIndex(indexOfSelectedPipeline, selectedModel)
+      : null;
+
+  const indexOfSelectedOption =
+    selectedPipeline && selectedModel ? `${indexOfSelectedPipeline},${indexOfSelectedModel}` : -1;
+
+  const handleSelectChange = (e) => {
+    const [pipelineIdx, modelIdx] = e.target.value.split(',').map(Number);
+
+    if (pipelineIdx === -1) {
+      setSelectedPipeline(null);
+      setSelectedModel(null);
+      return;
+    }
+
+    const selectedPipeline = availPipelines.pipelines[pipelineIdx];
+    const selectedModel = modelIdx !== undefined ? selectedPipeline.models[modelIdx] : null;
+
+    setSelectedPipeline(selectedPipeline.id);
+    setSelectedModel(selectedModel);
+  };
+
+  if (isValidating) {
+    return (
+      <Skeleton css={{ height: 20, width: 100 }} />
+    );
+  }
+
+  return (
+    <>
+      <Box
+        as="select"
+        value={indexOfSelectedOption}
+        onChange={handleSelectChange}
+      css={{
+        py: "$1",
+        pl: "$2",
+        border: "none",
+        bc: "$panel",
+        appearance: "none",
+        pr: "$5",
+        maxWidth: "50%", // Ensure the dropdown doesn't exceed the container width
+        "@bp2": {
+          maxWidth: "100%", // Remove margin-bottom for larger screens
+        },
+      }}
+    >
+      <Box as="option" key="-1" value={-1}>
+        Transcoding
+      </Box>
+      {availPipelines?.pipelines.length === 0 ? (
+        <Box as="option" disabled>
+          AI Pipelines Unavailable
+        </Box>
+      ) : (
+        <>
+          <Box as="option" disabled 
+            css={{
+              py: "$1",
+              pl: "$2",
+              border: "none",
+              bc: "$panel",
+              appearance: "none",
+              pr: "$5",
+            }}>
+            ----------------
+          </Box>
+          <Box as="option" disabled 
+            css={{
+              py: "$1",
+              pl: "$2",
+              border: "none",
+              bc: "$panel",
+              appearance: "none",
+              pr: "$5",
+            }}>
+            AI Pipelines
+          </Box>
+          <Box as="option" disabled 
+            css={{
+              py: "$1",
+              pl: "$2",
+              border: "none",
+              bc: "$panel",
+              appearance: "none",
+              pr: "$5",
+            }}>
+            ----------------
+          </Box>
+          {availPipelines?.pipelines.map((p, pindex) => (
+            p.models?.map((m, mindex) => (
+              <Box as="option" key={`${p.id}-${m}`} value={`${pindex},${mindex}`}>
+                {p.id} - {m}
+              </Box>
+            ))
+          ))}
+        </>
+      )}
+    </Box>
+    <Box
+    as={ChevronDownIcon}
+    css={{
+      pointerEvents: "none",
+    }}
+    />
+    </>
+  );
+};
+
+export default PerformanceListSelector;
--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -1,4 +1,5 @@
 import PerformanceList from "@components/PerformanceList";
+import PerformanceListSelector from '@components/PerformanceListSelector';
 import { getLayout, LAYOUT_MAX_WIDTH } from "@layouts/main";
 import { getOrchestrators } from "@lib/api/ssr";
 import { EnsIdentity } from "@lib/api/types/get-ens";
@@ -6,8 +7,10 @@ import { Box, Container, Flex, Heading } from "@livepeer/design-system";
 import { ChevronDownIcon } from "@modulz/radix-icons";
 import Head from "next/head";
 import { useState } from "react";
-import { ALL_REGIONS } from "utils/allRegions";
 import { getApollo, OrchestratorsQueryResult } from "../apollo";
+import { Pipeline } from "@lib/api/types/get-available-pipelines";
+import { useRegionsData } from "hooks/useSwr";
+import { Region } from "@lib/api/types/get-regions";
 
 type PageProps = {
   orchestratorIds: Pick<
@@ -18,7 +21,10 @@ type PageProps = {
 };
 
 const LeaderboardPage = ({ orchestratorIds }: PageProps) => {
-  const [region, setRegion] = useState<keyof typeof ALL_REGIONS>("global");
+  const [selectedPipeline, setSelectedPipeline] = useState<Pipeline["id"] | null>(null);
+  const [selectedModel, setSelectedModel] = useState<string | null>(null);
+  const knownRegions = useRegionsData();
+  const [region, setRegion] = useState<Region["id"]>("GLOBAL");
 
   return (
     <>
@@ -33,57 +39,98 @@ const LeaderboardPage = ({ orchestratorIds }: PageProps) => {
             width: "100%",
           }}
         >
-          <Flex align="center" justify="between" css={{ mb: "$4" }}>
+          <Flex justify="between" 
+            css={{ 
+              width: "100%",
+              flexDirection: "column", // Default to column for mobile
+              mb: "0",
+              "@bp2": {
+                flexDirection: "row", // Change to row for larger screens
+                mb: "$4",
+              },
+            }}
+          >
             <Heading
               size="3"
               as="h1"
               css={{
                 fontWeight: 700,
-                "@bp2": {
-                  fontSize: 26,
+                fontSize: 26,
+                mb: "$2",
+                "@bp3": {
+                  mb: "0",
                 },
               }}
             >
               Performance Leaderboard
             </Heading>
-            <Flex css={{ fontSize: "$2" }} align="center">
-              <Flex css={{ mr: "$2" }}>Region:</Flex>
+            <Flex
+              css={{
+                fontSize: "$2",
+                flexDirection: "column", // Default to column for mobile
+                "@bp2": {
+                  flexDirection: "row", // Change to row for larger screens
+                },
+              }}
+            >
               <Flex
-                align="center"
                 css={{
-                  bc: "$panel",
-                  borderRadius: "$2",
-                  border: "1px solid $neutral5",
-                  position: "relative",
+                  flexDirection: "row", // Stack title and dropdown vertically on mobile
+                  alignItems: "center",
+                  mb: "$2", // Add margin-bottom for spacing when stacked
+                  ml: "$2", // Add margin-left for spacing between groups
+                  "@bp2": {
+                    mb: "0", // Remove margin-bottom for larger screens
+                    mr: "$4", // Add margin-right for spacing between groups
+                  },
                 }}
               >
-                <Box
-                  as="select"
-                  onChange={(e) => {
-                    setRegion(e.target.value as keyof typeof ALL_REGIONS);
-                  }}
-                  css={{
-                    py: "$1",
-                    pl: "$2",
-                    border: "none",
-                    bc: "transparent",
-                    appearance: "none",
-                    pr: "$5",
-                  }}
-                >
-                  {Object.entries(ALL_REGIONS).map(([key, value]) => (
-                    <Box as="option" key={key} value={key}>
-                      {value}
-                    </Box>
-                  ))}
-                </Box>
-                <Box
-                  as={ChevronDownIcon}
-                  css={{
-                    pointerEvents: "none",
-                    right: 6,
-                    position: "absolute",
-                  }}
+                <Flex css={{ mr: "$2" }}>Region:</Flex>
+                  <Box
+                    as="select"
+                    onChange={(e) => {
+                      setRegion(e.target.value as Region["id"]);
+                    }}
+                    css={{
+                      py: "$1",
+                      pl: "$2",
+                      border: "none",
+                      bc: "$panel",
+                      appearance: "none",
+                    }}
+                  >
+                    {knownRegions?.regions
+                    .filter((r) => r.type === (selectedPipeline?"ai":"transcoding"))
+                    .map((region) => {
+                      return (<Box as="option" key={region.id} value={region.id}>
+                        {region.name}
+                      </Box>)
+                    })}
+                  </Box>
+                  <Box
+                    as={ChevronDownIcon}
+                    css={{
+                      pointerEvents: "none",
+                      }}
+                  />
+                </Flex>
+              <Flex
+                css={{
+                  flexDirection: "row", // Align title and dropdown horizontally
+                  alignItems: "center",
+                  mb: "$2", // Add margin-bottom for spacing when stacked
+                  ml: "$2", // Add margin-left for spacing between groups
+                  "@bp2": {
+                    mb: "0", // Remove margin-bottom for larger screens
+                  },
+                }}
+              >
+                <Flex css={{ mr: "$2" }}>Type:</Flex>
+                <PerformanceListSelector
+                  selectedPipeline={selectedPipeline}
+                  setSelectedPipeline={setSelectedPipeline}
+                  selectedModel={selectedModel}
+                  setSelectedModel={setSelectedModel}
                 />
               </Flex>
             </Flex>
@@ -93,6 +140,8 @@ const LeaderboardPage = ({ orchestratorIds }: PageProps) => {
               data={orchestratorIds}
               pageSize={20}
               region={region}
+              pipeline={selectedPipeline}
+              model={selectedModel}
             />
           </Box>
         </Flex>
