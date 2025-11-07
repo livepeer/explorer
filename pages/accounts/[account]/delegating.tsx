de8d457bfda09ef42192826f9ccc2efaaf441afb
ECWireless <elliott@coopallc.com>
2025-11-07 08:13:23 -0700

    test: remove paths from delegating page

--- pages/accounts/[account]/delegating.tsx
+++ pages/accounts/[account]/delegating.tsx
@@ -1,15 +1,8 @@
-import { getSortedOrchestrators } from "@lib/api/ssr";
-
 const Delegating = () => <div>Test</div>;
 
 export const getStaticPaths = async () => {
-  const { sortedOrchestrators } = await getSortedOrchestrators();
-
   return {
-    paths:
-      sortedOrchestrators?.data?.transcoders?.map((t) => ({
-        params: { account: t.id },
-      })) ?? [],
+    paths: [],
     fallback: "blocking",
   };
 };
