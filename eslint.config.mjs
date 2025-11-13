5c0942bd605c144c440f01c4c4742bc59075e1b7
Rick Staa <rick.staa@outlook.com>
2025-11-13 12:26:49 +0100

    refactor: move eslint rule to config file
    Co-authored-by: Jip Stavenuiter <jipstavenuiter@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,9 @@ import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 export default [
   {
     ignores: ["node_modules/**", ".next/**", "dist/**"],
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
   },
   ...nextCoreWebVitals,
 ];
