4924d1d8eaa58592f09ea374969f6a4bb775f164
ECWireless <elliott@coopallc.com>
2025-12-04 15:56:06 -0700

    test: remove getStaticPaths on delegating page

--- pages/accounts/[account]/delegating.tsx
+++ pages/accounts/[account]/delegating.tsx
@@ -1,17 +1,3 @@
-import { getSortedOrchestrators } from "@lib/api/ssr";
-
 const Delegating = () => <div>Test</div>;
-
-export const getStaticPaths = async () => {
-  const { sortedOrchestrators } = await getSortedOrchestrators();
-
-  return {
-    paths:
-      sortedOrchestrators?.data?.transcoders?.map((t) => ({
-        params: { account: t.id },
-      })) ?? [],
-    fallback: "blocking",
-  };
-};
 
 export default Delegating;
