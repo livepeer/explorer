ab36beee09a6c21fe54d87c9fb244e4b9f00641c
ECWireless <elliott@coopallc.com>
2025-12-04 18:00:52 -0700

    test: pass empty props on delegating page

--- pages/accounts/[account]/delegating.tsx
+++ pages/accounts/[account]/delegating.tsx
@@ -1,18 +1,6 @@
 // import AccountLayout from "@layouts/account";
 // import { getLayout } from "@layouts/main";
-import { getAccount, getSortedOrchestrators } from "@lib/api/ssr";
-import { EnsIdentity } from "@lib/api/types/get-ens";
-import {
-  AccountQueryResult,
-  getApollo,
-  OrchestratorsSortedQueryResult,
-} from "apollo";
-
-type PageProps = {
-  account: AccountQueryResult["data"] | null;
-  sortedOrchestrators: OrchestratorsSortedQueryResult["data"];
-  fallback: { [key: string]: EnsIdentity };
-};
+import { getSortedOrchestrators } from "@lib/api/ssr";
 
 const Delegating = () => <div>Test</div>;
 
@@ -28,32 +16,10 @@ export const getStaticPaths = async () => {
   };
 };
 
-export const getStaticProps = async (context) => {
+export const getStaticProps = async () => {
   try {
-    const client = getApollo();
-    const { account, fallback } = await getAccount(
-      client,
-      context.params?.account?.toString().toLowerCase()
-    );
-
-    const { sortedOrchestrators, fallback: sortedOrchestratorsFallback } =
-      await getSortedOrchestrators(client);
-
-    if (!sortedOrchestrators.data) {
-      return { notFound: true, revalidate: 300 };
-    }
-
-    const props: PageProps = {
-      account: account.data,
-      sortedOrchestrators: sortedOrchestrators.data,
-      fallback: {
-        ...sortedOrchestratorsFallback,
-        ...fallback,
-      },
-    };
-
     return {
-      props,
+      props: {},
       revalidate: 600,
     };
   } catch (e) {
