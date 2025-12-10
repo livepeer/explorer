da3a5790371b342313e71217178468a172a047a1
ECWireless <elliott@coopallc.com>
2025-12-10 11:26:24 -0700

    fix: temp move account pages to client-side

--- layouts/account.tsx
+++ layouts/account.tsx
@@ -4,8 +4,10 @@ import DelegatingWidget from "@components/DelegatingWidget";
 import HistoryView from "@components/HistoryView";
 import OrchestratingView from "@components/OrchestratingView";
 import Profile from "@components/Profile";
+import Spinner from "@components/Spinner";
 import { getLayout, LAYOUT_MAX_WIDTH } from "@layouts/main";
 import { bondingManager } from "@lib/api/abis/main/BondingManager";
+import { getAccount, getSortedOrchestrators } from "@lib/api/ssr";
 import { checkAddressEquality } from "@lib/utils";
 import {
   Box,
@@ -19,11 +21,13 @@ import {
 } from "@livepeer/design-system";
 import {
   AccountQueryResult,
+  getApollo,
   OrchestratorsSortedQueryResult,
   useAccountQuery,
 } from "apollo";
 import { useBondingManagerAddress } from "hooks/useContracts";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 import { useRouter } from "next/router";
 import { useEffect, useMemo, useState } from "react";
 import { useWindowSize } from "react-use";
@@ -41,13 +45,36 @@ type TabTypeEnum = "delegating" | "orchestrating" | "history";
 
 const ACCOUNT_VIEWS: TabTypeEnum[] = ["delegating", "orchestrating", "history"];
 
-const AccountLayout = ({
-  account,
-  sortedOrchestrators,
-}: {
-  account?: AccountQueryResult["data"] | null;
-  sortedOrchestrators: OrchestratorsSortedQueryResult["data"];
-}) => {
+const AccountLayout = () => {
+  /* PART OF https://github.com/livepeer/explorer/pull/427 - REMOVE ONCE SERVER-SIDE ISSUE IS FIXED */
+  const context = { params: useParams() };
+  const [sortedOrchestrators, setSortedOrchestrators] = useState<
+    OrchestratorsSortedQueryResult["data"] | null
+  >(null);
+  const [account, setAccount] = useState<AccountQueryResult["data"] | null>(
+    null
+  );
+
+  useEffect(() => {
+    (async () => {
+      const { sortedOrchestrators: _sortedOrchestrators } =
+        await getSortedOrchestrators();
+
+      const client = getApollo();
+      const { account } = await getAccount(
+        client,
+        context.params?.account?.toString().toLowerCase() ?? ""
+      );
+
+      setSortedOrchestrators(
+        _sortedOrchestrators.data as OrchestratorsSortedQueryResult["data"]
+      );
+      setAccount(account.data);
+    })();
+  }, [context.params?.account]);
+
+  /* ************* */
+
   const accountAddress = useAccountAddress();
   const { width } = useWindowSize();
   const router = useRouter();
@@ -133,6 +160,26 @@ const AccountLayout = ({
   useEffect(() => {
     setSelectedStakingAction("delegate");
   }, [setSelectedStakingAction]);
+
+  /* PART OF https://github.com/livepeer/explorer/pull/427 - REMOVE ONCE SERVER-SIDE ISSUE IS FIXED */
+  if (!sortedOrchestrators || !account) {
+    return (
+      <Flex
+        css={{
+          height: "calc(100vh - 100px)",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          "@bp3": {
+            height: "100vh",
+          },
+        }}
+      >
+        <Spinner />
+      </Flex>
+    );
+  }
+  /* ************* */
 
   return (
     <Container css={{ maxWidth: LAYOUT_MAX_WIDTH, width: "100%" }}>
