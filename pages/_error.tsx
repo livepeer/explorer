add0007dd81007614b6982e3ce4aa3cf4c95a479
ECWireless <elliott@coopallc.com>
2025-12-10 19:07:07 -0700

    feat: more consistent getStaticProps errors

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,35 +1,6 @@
-import { Box, Flex } from "@livepeer/design-system";
-
-const statusCodes: { [code: number]: string } = {
-  400: "Bad Request",
-  405: "Method Not Allowed",
-  500: "Internal Server Error",
-};
-
+import ErrorComponent from "@components/Error";
 function Error({ statusCode }) {
-  return (
-    <Flex
-      css={{
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-        bc: "$loContrast",
-      }}
-    >
-      <Box
-        css={{
-          fontSize: "$5",
-          pr: "$4",
-          mr: "$4",
-          borderRight: "1px solid",
-          borderColor: "$hiContrast",
-        }}
-      >
-        {statusCode}
-      </Box>
-      <p>{statusCodes[statusCode]}</p>
-    </Flex>
-  );
+  return <ErrorComponent statusCode={statusCode} />;
 }
 
 Error.getInitialProps = ({ res, err }) => {
