68a6f07dd556e72b3c5a644a18fd264bd5643574
ECWireless <40322776+ECWireless@users.noreply.github.com>
2025-12-12 08:52:55 -0700

    fix: remove transpile package array in next config (#427)
    * fix: remove transpilePackages array in next config
    
    * fix: transpile @vanilla-extract/sprinkles
    
    * fix: temp move account pages to client-side
    
    * fix: separate orch and account fetching in effect
    
    * feat: use useSWR for account data fetching

--- pages/accounts/[account]/delegating.tsx
+++ pages/accounts/[account]/delegating.tsx
@@ -1,70 +1,8 @@
 import AccountLayout from "@layouts/account";
 import { getLayout } from "@layouts/main";
-import { getAccount, getSortedOrchestrators } from "@lib/api/ssr";
-import { EnsIdentity } from "@lib/api/types/get-ens";
-import {
-  AccountQueryResult,
-  getApollo,
-  OrchestratorsSortedQueryResult,
-} from "apollo";
 
-type PageProps = {
-  account: AccountQueryResult["data"] | null;
-  sortedOrchestrators: OrchestratorsSortedQueryResult["data"];
-  fallback: { [key: string]: EnsIdentity };
-};
-
-const Delegating = ({ account, sortedOrchestrators }: PageProps) => (
-  <AccountLayout sortedOrchestrators={sortedOrchestrators} account={account} />
-);
+const Delegating = () => <AccountLayout />;
 
 Delegating.getLayout = getLayout;
-
-export const getStaticPaths = async () => {
-  const { sortedOrchestrators } = await getSortedOrchestrators();
-
-  return {
-    paths:
-      sortedOrchestrators?.data?.transcoders?.map((t) => ({
-        params: { account: t.id },
-      })) ?? [],
-    fallback: "blocking",
-  };
-};
-
-export const getStaticProps = async (context) => {
-  try {
-    const client = getApollo();
-    const { account, fallback } = await getAccount(
-      client,
-      context.params?.account?.toString().toLowerCase()
-    );
-
-    const { sortedOrchestrators, fallback: sortedOrchestratorsFallback } =
-      await getSortedOrchestrators(client);
-
-    if (!sortedOrchestrators.data) {
-      return { notFound: true, revalidate: 300 };
-    }
-
-    const props: PageProps = {
-      account: account.data,
-      sortedOrchestrators: sortedOrchestrators.data,
-      fallback: {
-        ...sortedOrchestratorsFallback,
-        ...fallback,
-      },
-    };
-
-    return {
-      props,
-      revalidate: 600,
-    };
-  } catch (e) {
-    console.error(e);
-  }
-
-  return { notFound: true, revalidate: 300 };
-};
 
 export default Delegating;
