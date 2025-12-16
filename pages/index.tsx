1eddd071ff20f5acc4c1f6581ed65e9644034804
ECWireless <elliott@coopallc.com>
2025-12-16 08:51:39 -0700

    test: remove hooks import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import "react-circular-progressbar/dist/styles.css";
 
 import ErrorComponent from "@components/Error";
-import ExplorerChart from "@components/ExplorerChart";
+// import ExplorerChart from "@components/ExplorerChart";
 import OrchestratorList from "@components/OrchestratorList";
 import RoundStatus from "@components/RoundStatus";
 import Spinner from "@components/Spinner";
 import TransactionsList, {
   FILTERED_EVENT_TYPENAMES,
 } from "@components/TransactionsList";
-import { HomeChartData } from "@lib/api/types/get-chart-data";
+// import { HomeChartData } from "@lib/api/types/get-chart-data";
 import { EnsIdentity } from "@lib/api/types/get-ens";
 import {
   Box,
@@ -19,9 +19,9 @@ import {
   Link as A,
 } from "@livepeer/design-system";
 import { ArrowRightIcon } from "@modulz/radix-icons";
-import { useChartData } from "hooks";
+// import { useChartData } from "hooks";
 import Link from "next/link";
-import { useMemo, useState } from "react";
+import { useMemo /* useState  */ } from "react";
 
 import {
   EventsQueryResult,
@@ -31,196 +31,196 @@ import {
 } from "../apollo";
 import { getEvents, getOrchestrators, getProtocol } from "../lib/api/ssr";
 
-const Panel = ({ children }) => (
-  <Flex
-    css={{
-      minHeight: 240,
-      height: 240,
-      padding: "24px",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      border: "0.5px solid $colors$neutral4",
-      flex: 1,
-      width: "100%",
-      minWidth: 350,
-    }}
-  >
-    {children}
-  </Flex>
-);
+// const Panel = ({ children }) => (
+//   <Flex
+//     css={{
+//       minHeight: 240,
+//       height: 240,
+//       padding: "24px",
+//       flexDirection: "column",
+//       alignItems: "center",
+//       justifyContent: "center",
+//       border: "0.5px solid $colors$neutral4",
+//       flex: 1,
+//       width: "100%",
+//       minWidth: 350,
+//     }}
+//   >
+//     {children}
+//   </Flex>
+// );
 
-const Charts = ({ chartData }: { chartData: HomeChartData | null }) => {
-  const [feesPaidGrouping, setFeesPaidGrouping] = useState<"day" | "week">(
-    "week"
-  );
-  const feesPaidData = useMemo(
-    () =>
-      (feesPaidGrouping === "day"
-        ? chartData?.dayData?.map((day) => ({
-            x: Number(day.dateS),
-            y: Number(day.volumeUsd),
-          }))
-        : chartData?.weeklyData?.map((week) => ({
-            x: Number(week.date),
-            y: Number(week.weeklyVolumeUsd),
-          }))) ?? [],
-    [feesPaidGrouping, chartData]
-  );
+// const Charts = ({ chartData }: { chartData: HomeChartData | null }) => {
+//   const [feesPaidGrouping, setFeesPaidGrouping] = useState<"day" | "week">(
+//     "week"
+//   );
+//   const feesPaidData = useMemo(
+//     () =>
+//       (feesPaidGrouping === "day"
+//         ? chartData?.dayData?.map((day) => ({
+//             x: Number(day.dateS),
+//             y: Number(day.volumeUsd),
+//           }))
+//         : chartData?.weeklyData?.map((week) => ({
+//             x: Number(week.date),
+//             y: Number(week.weeklyVolumeUsd),
+//           }))) ?? [],
+//     [feesPaidGrouping, chartData]
+//   );
 
-  const [usageGrouping, setUsageGrouping] = useState<"day" | "week">("week");
-  const usageData = useMemo(
-    () =>
-      (usageGrouping === "day"
-        ? chartData?.dayData?.map((day) => ({
-            x: Number(day.dateS),
-            y: Number(day.feeDerivedMinutes),
-          }))
-        : chartData?.weeklyData?.map((week) => ({
-            x: Number(week.date),
-            y: Number(week.weeklyUsageMinutes),
-          }))) ?? [],
-    [usageGrouping, chartData]
-  );
+//   const [usageGrouping, setUsageGrouping] = useState<"day" | "week">("week");
+//   const usageData = useMemo(
+//     () =>
+//       (usageGrouping === "day"
+//         ? chartData?.dayData?.map((day) => ({
+//             x: Number(day.dateS),
+//             y: Number(day.feeDerivedMinutes),
+//           }))
+//         : chartData?.weeklyData?.map((week) => ({
+//             x: Number(week.date),
+//             y: Number(week.weeklyUsageMinutes),
+//           }))) ?? [],
+//     [usageGrouping, chartData]
+//   );
 
-  const participationRateData = useMemo(
-    () =>
-      chartData?.dayData?.slice(1)?.map((day) => ({
-        x: Number(day.dateS),
-        y: Number(day.participationRate),
-      })) ?? [],
-    [chartData]
-  );
-  const inflationRateData = useMemo(
-    () =>
-      chartData?.dayData?.slice(1)?.map((day) => ({
-        x: Number(day.dateS),
-        y: Number(day?.inflation ?? 0) / 1000000000,
-      })) ?? [],
-    [chartData]
-  );
-  const delegatorsCountData = useMemo(
-    () =>
-      chartData?.dayData?.slice(1)?.map((day) => ({
-        x: Number(day.dateS),
-        y: Number(day.delegatorsCount),
-      })) ?? [],
-    [chartData]
-  );
-  const activeTranscoderCountData = useMemo(
-    () =>
-      chartData?.dayData?.slice(1)?.map((day) => ({
-        x: Number(day.dateS),
-        y: Number(day.activeTranscoderCount),
-      })) ?? [],
-    [chartData]
-  );
+//   const participationRateData = useMemo(
+//     () =>
+//       chartData?.dayData?.slice(1)?.map((day) => ({
+//         x: Number(day.dateS),
+//         y: Number(day.participationRate),
+//       })) ?? [],
+//     [chartData]
+//   );
+//   const inflationRateData = useMemo(
+//     () =>
+//       chartData?.dayData?.slice(1)?.map((day) => ({
+//         x: Number(day.dateS),
+//         y: Number(day?.inflation ?? 0) / 1000000000,
+//       })) ?? [],
+//     [chartData]
+//   );
+//   const delegatorsCountData = useMemo(
+//     () =>
+//       chartData?.dayData?.slice(1)?.map((day) => ({
+//         x: Number(day.dateS),
+//         y: Number(day.delegatorsCount),
+//       })) ?? [],
+//     [chartData]
+//   );
+//   const activeTranscoderCountData = useMemo(
+//     () =>
+//       chartData?.dayData?.slice(1)?.map((day) => ({
+//         x: Number(day.dateS),
+//         y: Number(day.activeTranscoderCount),
+//       })) ?? [],
+//     [chartData]
+//   );
 
-  return (
-    <>
-      <Panel>
-        <ExplorerChart
-          tooltip={`The amount of ${
-            feesPaidGrouping === "day" ? "daily" : "weekly"
-          } fees in dollars which have been historically paid out using the protocol.`}
-          data={
-            feesPaidGrouping === "week"
-              ? feesPaidData.slice(-26)
-              : feesPaidData.slice(-183)
-          }
-          base={Number(
-            (feesPaidGrouping === "day"
-              ? chartData?.oneDayVolumeUSD
-              : chartData?.oneWeekVolumeUSD) ?? 0
-          )}
-          basePercentChange={Number(
-            (feesPaidGrouping === "day"
-              ? chartData?.volumeChangeUSD
-              : chartData?.weeklyVolumeChangeUSD) ?? 0
-          )}
-          title={`Fees Paid ${feesPaidGrouping === "day" ? "(1d)" : "(7d)"}`}
-          unit="usd"
-          type="bar"
-          grouping={feesPaidGrouping}
-          onToggleGrouping={setFeesPaidGrouping}
-        />
-      </Panel>
-      <Panel>
-        <ExplorerChart
-          tooltip="The percent of LPT which has been delegated to an orchestrator."
-          data={participationRateData}
-          base={Number(chartData?.participationRate ?? 0)}
-          basePercentChange={Number(chartData?.participationRateChange ?? 0)}
-          title="Participation Rate"
-          unit="percent"
-          type="line"
-        />
-      </Panel>
-      <Panel>
-        <ExplorerChart
-          tooltip="The percent of LPT which is minted each round as rewards for delegators/orchestrators on the network."
-          data={inflationRateData}
-          base={Number(chartData?.inflation ?? 0) / 1000000000}
-          basePercentChange={Number(chartData?.inflationChange ?? 0)}
-          title="Inflation Rate"
-          unit="small-percent"
-          type="line"
-        />
-      </Panel>
-      <Panel>
-        <ExplorerChart
-          tooltip={`The ${
-            usageGrouping === "day" ? "daily" : "weekly"
-          } usage of the network in minutes.`}
-          data={
-            usageGrouping === "week"
-              ? usageData.slice(-26)
-              : usageData.slice(-183)
-          }
-          base={Number(
-            (usageGrouping === "day"
-              ? chartData?.oneDayUsage
-              : chartData?.oneWeekUsage) ?? 0
-          )}
-          basePercentChange={Number(
-            (usageGrouping === "day"
-              ? chartData?.dailyUsageChange
-              : chartData?.weeklyUsageChange) ?? 0
-          )}
-          title={`Estimated Usage ${usageGrouping === "day" ? "(1d)" : "(7d)"}`}
-          unit="minutes"
-          type="bar"
-          grouping={usageGrouping}
-          onToggleGrouping={setUsageGrouping}
-        />
-      </Panel>
-      <Panel>
-        <ExplorerChart
-          tooltip="The count of delegators participating in the network."
-          data={delegatorsCountData}
-          base={Number(chartData?.delegatorsCount ?? 0)}
-          basePercentChange={Number(chartData?.delegatorsCountChange ?? 0)}
-          title="Delegators"
-          unit="small-unitless"
-          type="line"
-        />
-      </Panel>
-      <Panel>
-        <ExplorerChart
-          tooltip="The number of orchestrators providing transcoding services to the network."
-          data={activeTranscoderCountData}
-          base={Number(chartData?.activeTranscoderCount ?? 0)}
-          basePercentChange={Number(
-            chartData?.activeTranscoderCountChange ?? 0
-          )}
-          title="Orchestrators"
-          unit="none"
-          type="line"
-        />
-      </Panel>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <Panel>
+//         <ExplorerChart
+//           tooltip={`The amount of ${
+//             feesPaidGrouping === "day" ? "daily" : "weekly"
+//           } fees in dollars which have been historically paid out using the protocol.`}
+//           data={
+//             feesPaidGrouping === "week"
+//               ? feesPaidData.slice(-26)
+//               : feesPaidData.slice(-183)
+//           }
+//           base={Number(
+//             (feesPaidGrouping === "day"
+//               ? chartData?.oneDayVolumeUSD
+//               : chartData?.oneWeekVolumeUSD) ?? 0
+//           )}
+//           basePercentChange={Number(
+//             (feesPaidGrouping === "day"
+//               ? chartData?.volumeChangeUSD
+//               : chartData?.weeklyVolumeChangeUSD) ?? 0
+//           )}
+//           title={`Fees Paid ${feesPaidGrouping === "day" ? "(1d)" : "(7d)"}`}
+//           unit="usd"
+//           type="bar"
+//           grouping={feesPaidGrouping}
+//           onToggleGrouping={setFeesPaidGrouping}
+//         />
+//       </Panel>
+//       <Panel>
+//         <ExplorerChart
+//           tooltip="The percent of LPT which has been delegated to an orchestrator."
+//           data={participationRateData}
+//           base={Number(chartData?.participationRate ?? 0)}
+//           basePercentChange={Number(chartData?.participationRateChange ?? 0)}
+//           title="Participation Rate"
+//           unit="percent"
+//           type="line"
+//         />
+//       </Panel>
+//       <Panel>
+//         <ExplorerChart
+//           tooltip="The percent of LPT which is minted each round as rewards for delegators/orchestrators on the network."
+//           data={inflationRateData}
+//           base={Number(chartData?.inflation ?? 0) / 1000000000}
+//           basePercentChange={Number(chartData?.inflationChange ?? 0)}
+//           title="Inflation Rate"
+//           unit="small-percent"
+//           type="line"
+//         />
+//       </Panel>
+//       <Panel>
+//         <ExplorerChart
+//           tooltip={`The ${
+//             usageGrouping === "day" ? "daily" : "weekly"
+//           } usage of the network in minutes.`}
+//           data={
+//             usageGrouping === "week"
+//               ? usageData.slice(-26)
+//               : usageData.slice(-183)
+//           }
+//           base={Number(
+//             (usageGrouping === "day"
+//               ? chartData?.oneDayUsage
+//               : chartData?.oneWeekUsage) ?? 0
+//           )}
+//           basePercentChange={Number(
+//             (usageGrouping === "day"
+//               ? chartData?.dailyUsageChange
+//               : chartData?.weeklyUsageChange) ?? 0
+//           )}
+//           title={`Estimated Usage ${usageGrouping === "day" ? "(1d)" : "(7d)"}`}
+//           unit="minutes"
+//           type="bar"
+//           grouping={usageGrouping}
+//           onToggleGrouping={setUsageGrouping}
+//         />
+//       </Panel>
+//       <Panel>
+//         <ExplorerChart
+//           tooltip="The count of delegators participating in the network."
+//           data={delegatorsCountData}
+//           base={Number(chartData?.delegatorsCount ?? 0)}
+//           basePercentChange={Number(chartData?.delegatorsCountChange ?? 0)}
+//           title="Delegators"
+//           unit="small-unitless"
+//           type="line"
+//         />
+//       </Panel>
+//       <Panel>
+//         <ExplorerChart
+//           tooltip="The number of orchestrators providing transcoding services to the network."
+//           data={activeTranscoderCountData}
+//           base={Number(chartData?.activeTranscoderCount ?? 0)}
+//           basePercentChange={Number(
+//             chartData?.activeTranscoderCountChange ?? 0
+//           )}
+//           title="Orchestrators"
+//           unit="none"
+//           type="line"
+//         />
+//       </Panel>
+//     </>
+//   );
+// };
 
 type PageProps = {
   hadError: boolean;
@@ -244,7 +244,7 @@ const Home = ({ hadError, orchestrators, events, protocol }: PageProps) => {
     [events]
   );
 
-  const chartData = useChartData();
+  // const chartData = useChartData();
 
   if (hadError) {
     return <ErrorComponent statusCode={500} />;
@@ -299,7 +299,7 @@ const Home = ({ hadError, orchestrators, events, protocol }: PageProps) => {
                 overflowX: "auto",
               }}
             >
-              <Flex>
+              {/* <Flex>
                 <Box
                   css={{
                     width: "100%",
@@ -309,7 +309,7 @@ const Home = ({ hadError, orchestrators, events, protocol }: PageProps) => {
                 >
                   <Charts chartData={chartData} />
                 </Box>
-              </Flex>
+              </Flex> */}
               <Flex
                 css={{
                   justifyContent: "center",
