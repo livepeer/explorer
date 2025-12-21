fa98ecadf505026a4e8a7c02e9f736a5b394555d
ECWireless <40322776+ECWireless@users.noreply.github.com>
2025-12-21 14:13:09 -0600

    fix: reverse order of internal transaction events (#455)
    * fix: reverse order of internal transaction events
    
    * fix: return undefined events as empty array
    
    * fix: remove unnecessary undefined events check

--- components/HistoryView/index.tsx
+++ components/HistoryView/index.tsx
@@ -38,10 +38,16 @@ const Index = () => {
     }
   );
 
-  const events = useMemo(
-    () => data?.transactions?.flatMap(({ events: e }) => e ?? []) ?? [],
-    [data]
-  );
+  const events = useMemo(() => {
+    // First reverse the order of the array of events per transaction to have events in descending order
+    const reversedEvents = data?.transactions?.map((tx) => {
+      return {
+        ...tx,
+        events: tx.events ? tx.events.slice().reverse() : [],
+      };
+    });
+    return reversedEvents?.flatMap(({ events: e }) => e ?? []) ?? [];
+  }, [data]);
 
   const lastEventTimestamp = useMemo(
     () =>
