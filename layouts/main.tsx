4879a68c77f9b706efbfb86fa8a310dcc1723a3d
ECWireless <elliott@coopallc.com>
2026-01-07 08:40:06 -0700

    test: comment out components with rainbowkit

--- layouts/main.tsx
+++ layouts/main.tsx
@@ -1,15 +1,15 @@
 import AppBar from "@components/AppBar";
-import Claim from "@components/Claim";
-import ConnectButton from "@components/ConnectButton";
+// import Claim from "@components/Claim";
+// import ConnectButton from "@components/ConnectButton";
 import Drawer from "@components/Drawer";
 import Hamburger from "@components/Hamburger";
 import InactiveWarning from "@components/InactiveWarning";
 import Logo from "@components/Logo";
 import PopoverLink from "@components/PopoverLink";
 import ProgressBar from "@components/ProgressBar";
-import RegisterToVote from "@components/RegisterToVote";
+// import RegisterToVote from "@components/RegisterToVote";
 import Search from "@components/Search";
-import TxConfirmedDialog from "@components/TxConfirmedDialog";
+// import TxConfirmedDialog from "@components/TxConfirmedDialog";
 import TxStartedDialog from "@components/TxStartedDialog";
 import TxSummaryDialog from "@components/TxSummaryDialog";
 import URLVerificationBanner from "@components/URLVerificationBanner";
@@ -671,9 +671,9 @@ const Layout = ({ children, title = "Livepeer Explorer" }) => {
 
                       <Flex css={{ marginLeft: "auto" }}>
                         <ContractAddressesPopover activeChain={activeChain} />
-                        <Flex css={{ alignItems: "center", marginLeft: "8px" }}>
+                        {/* <Flex css={{ alignItems: "center", marginLeft: "8px" }}>
                           <ConnectButton showBalance={false} />
-                        </Flex>
+                        </Flex> */}
                         <Search />
                       </Flex>
                     </Flex>
@@ -690,18 +690,18 @@ const Layout = ({ children, title = "Livepeer Explorer" }) => {
                     {!asPath.includes("/migrate") && accountAddress && (
                       <InactiveWarning />
                     )}
-                    {!asPath?.includes("/migrate") && accountAddress && (
+                    {/* {!asPath?.includes("/migrate") && accountAddress && (
                       <Claim />
-                    )}
-                    {!asPath?.includes("/migrate") && accountAddress && (
+                    )} */}
+                    {/* {!asPath?.includes("/migrate") && accountAddress && (
                       <RegisterToVote />
-                    )}
+                    )} */}
                     {children}
                   </Box>
                 </Flex>
               </Box>
             </Box>
-            <TxConfirmedDialog />
+            {/* <TxConfirmedDialog /> */}
             <TxSummaryDialog />
             <TxStartedDialog />
             {latestTransaction && latestTransaction.step !== "confirmed" && (
