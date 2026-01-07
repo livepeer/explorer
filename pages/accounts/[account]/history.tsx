47f0c8bbb6a86d2b304fbaca6f78af123498f6b2
ECWireless <elliott@coopallc.com>
2026-01-07 08:55:19 -0700

    test: bring static paths back to account pages

--- pages/accounts/[account]/history.tsx
+++ pages/accounts/[account]/history.tsx
@@ -1,8 +1,70 @@
 import AccountLayout from "@layouts/account";
 import { getLayout } from "@layouts/main";
+import { getAccount, getSortedOrchestrators } from "@lib/api/ssr";
+import { EnsIdentity } from "@lib/api/types/get-ens";
+import {
+  AccountQueryResult,
+  getApollo,
+  OrchestratorsSortedQueryResult,
+} from "apollo";
 
-const History = () => <AccountLayout />;
+type PageProps = {
+  account: AccountQueryResult["data"];
+  sortedOrchestrators: OrchestratorsSortedQueryResult["data"];
+  fallback: { [key: string]: EnsIdentity };
+};
+
+const History = ({ account, sortedOrchestrators }: PageProps) => (
+  <AccountLayout sortedOrchestrators={sortedOrchestrators} account={account} />
+);
 
 History.getLayout = getLayout;
+
+export const getStaticPaths = async () => {
+  const { sortedOrchestrators } = await getSortedOrchestrators();
+
+  return {
+    paths:
+      sortedOrchestrators?.data?.transcoders?.map((t) => ({
+        params: { account: t.id },
+      })) ?? [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps = async (context) => {
+  try {
+    const client = getApollo();
+    const { account, fallback } = await getAccount(
+      client,
+      context.params?.account?.toString().toLowerCase()
+    );
+
+    const { sortedOrchestrators, fallback: sortedOrchestratorsFallback } =
+      await getSortedOrchestrators(client);
+
+    if (!account.data || !sortedOrchestrators.data) {
+      return { notFound: true, revalidate: 300 };
+    }
+
+    const props: PageProps = {
+      account: account.data,
+      sortedOrchestrators: sortedOrchestrators.data,
+      fallback: {
+        ...sortedOrchestratorsFallback,
+        ...fallback,
+      },
+    };
+
+    return {
+      props,
+      revalidate: 600,
+    };
+  } catch (e) {
+    console.error(e);
+  }
+
+  return { notFound: true, revalidate: 300 };
+};
 
 export default History;
