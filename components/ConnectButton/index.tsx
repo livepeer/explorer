0d90661daa46dcff9db4954e603df63f29389f34
ECWireless <elliott@coopallc.com>
2026-01-20 20:37:53 -0700

    fix: move inline styles to global CSS file

--- components/ConnectButton/index.tsx
+++ components/ConnectButton/index.tsx
@@ -2,18 +2,7 @@ import { ConnectButton as ConnectButtonRainbowKit } from "@rainbow-me/rainbowkit
 import { ConnectButtonProps } from "@rainbow-me/rainbowkit/dist/components/ConnectButton/ConnectButton";
 
 const ConnectButton = (props: ConnectButtonProps) => {
-  return (
-    <>
-      <style>{`
-        [data-rk] button[data-testid="rk-connect-button"] {
-          background-color: #113123 !important;
-          color: #4cc38a !important;
-        }
-      `}</style>
-
-      <ConnectButtonRainbowKit chainStatus="name" {...props} />
-    </>
-  );
+  return <ConnectButtonRainbowKit chainStatus="name" {...props} />;
 };
 
 export default ConnectButton;
