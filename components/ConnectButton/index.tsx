adba00f7e97a1bb9204da204b4c928d823da5be2
ECWireless <elliott@coopallc.com>
2026-01-20 20:07:34 -0700

    fix: make wallet modal accent text more readable

--- components/ConnectButton/index.tsx
+++ components/ConnectButton/index.tsx
@@ -2,7 +2,18 @@ import { ConnectButton as ConnectButtonRainbowKit } from "@rainbow-me/rainbowkit
 import { ConnectButtonProps } from "@rainbow-me/rainbowkit/dist/components/ConnectButton/ConnectButton";
 
 const ConnectButton = (props: ConnectButtonProps) => {
-  return <ConnectButtonRainbowKit chainStatus="name" {...props} />;
+  return (
+    <>
+      <style>{`
+        [data-rk] button[data-testid="rk-connect-button"] {
+          background-color: #113123 !important;
+          color: #4cc38a !important;
+        }
+      `}</style>
+
+      <ConnectButtonRainbowKit chainStatus="name" {...props} />
+    </>
+  );
 };
 
 export default ConnectButton;
